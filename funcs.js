2e7d9ceb658a373d6769309922d3ac56d9b624cb
slooi <s.looi2@outlook.com>
2020-04-02 01:44:28 +1300

    Simplify code

---
+++ funcs.js
@@ -0,0 +1,62 @@
+
+// FUNCTIONS
+function print(name,vaue){
+	console.log(name)
+	console.log(vaue)
+}
+
+function buildFramebuffer(texture){
+	const framebuffer = gl.createFramebuffer()
+	gl.bindFramebuffer(gl.FRAMEBUFFER,framebuffer)
+	gl.framebufferTexture2D(gl.FRAMEBUFFER,gl.COLOR_ATTACHMENT0,gl.TEXTURE_2D,texture,0)
+	return framebuffer
+}
+
+function buildTexture(pixels){
+	const texture = gl.createTexture()
+	gl.bindTexture(gl.TEXTURE_2D,texture)
+	gl.texImage2D(gl.TEXTURE_2D,0,format,width,height,0,format,texType,pixels)
+	
+	gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_MAG_FILTER,gl.NEAREST)
+	gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_MIN_FILTER,gl.NEAREST)
+	gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_WRAP_S,gl.CLAMP_TO_EDGE)
+	gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_WRAP_T,gl.CLAMP_TO_EDGE)
+
+	return texture
+}
+
+
+function createSrcData(){
+	const data = new Uint8Array(size)		//!@#!@#!@#
+	for(let i=0;i<size;i++){
+		data[i] = i%256
+	}
+	return data
+}
+
+function buildShader(type,source){
+	const shader = gl.createShader(type)
+	gl.shaderSource(shader,source)
+	gl.compileShader(shader)
+
+	if(!gl.getShaderParameter(shader,gl.COMPILE_STATUS)){
+		throw new Error('ERROR: compiling shader of type '+type+' Info: '+gl.getShaderInfoLog(shader))
+	}
+	return shader
+}
+
+function buildProgram(){
+	const program = gl.createProgram()
+	gl.attachShader(program,buildShader(gl.VERTEX_SHADER,vsSource))
+	gl.attachShader(program,buildShader(gl.FRAGMENT_SHADER,fsSource))
+	gl.linkProgram(program)
+	gl.validateProgram(program)
+
+	if(!gl.getProgramParameter(program,gl.LINK_STATUS)){
+		throw new Error('ERROR: linking program. Info: '+gl.getProgramInfoLog(program))
+	}
+	if(!gl.getProgramParameter(program,gl.VALIDATE_STATUS)){
+		throw new Error('ERROR: validating program. Info: '+gl.getProgramInfoLog(program))
+	}
+	return program
+}
---
+++ webgl.js
@@ -0,0 +1,105 @@
+console.log('loaded webgl.js')
+
+const vsSource = document.getElementById('vsSource').innerText
+const fsSource = document.getElementById('fsSource').innerText
+
+const size = 16
+const width = Math.sqrt(size/4)
+const height = width
+console.log('width,height',width,height)
+
+const canvas = document.createElement('canvas')
+canvas.width = width
+canvas.height = height
+document.body.append(canvas)
+
+const gl = canvas.getContext('webgl')
+
+const format = gl.RGBA
+const type = gl.FLOAT
+const texType = gl.UNSIGNED_BYTE
+
+// program
+const program = buildProgram()
+gl.useProgram(program)
+
+// locations
+// attrib
+const attribLocations = []
+for(let i=0;i<gl.getProgramParameter(program,gl.ACTIVE_ATTRIBUTES);i++){
+	const attribName = gl.getActiveAttrib(program,i).name
+	attribLocations[attribName] = gl.getAttribLocation(program,attribName)
+}
+// uniform
+const uniformLocations = []
+for(let i=0;i<gl.getProgramParameter(program,gl.ACTIVE_UNIFORMS);i++){
+	const uniformName = gl.getActiveUniform(program,i).name
+	uniformLocations[uniformName] = gl.getUniformLocation(program,uniformName)
+}
+
+// data
+let data = [
+//	x	y	
+	-1, -1, 0, 0,
+	1, -1, 1, 0,
+	-1,1, 0,1,
+	-1,1,0,1,
+	1,-1,1,0,
+	1,1,1,1
+]
+
+// buffer
+const positionBuffer = gl.createBuffer()
+gl.bindBuffer(gl.ARRAY_BUFFER,positionBuffer)
+gl.bufferData(gl.ARRAY_BUFFER,new Float32Array(data),gl.STATIC_DRAW)
+
+// pointer
+gl.vertexAttribPointer(
+	attribLocations.a_Position,
+	2,
+	gl.FLOAT,
+	0,
+	4*Float32Array.BYTES_PER_ELEMENT,
+	0*Float32Array.BYTES_PER_ELEMENT,
+)
+gl.enableVertexAttribArray(attribLocations.a_Position)
+
+gl.vertexAttribPointer(
+	attribLocations.a_TexCoord,
+	2,
+	gl.FLOAT,
+	0,
+	4*Float32Array.BYTES_PER_ELEMENT,
+	2*Float32Array.BYTES_PER_ELEMENT
+)
+gl.enableVertexAttribArray(attribLocations.a_TexCoord)
+
+// uniform
+
+
+
+// texture
+// src
+const srcData = createSrcData()
+print('srcData',srcData)
+const srcTex = buildTexture(srcData)
+
+// dst
+let dstTex = buildTexture(null)
+
+// frameBuffer
+const fb = buildFramebuffer(dstTex)
+//!@#!@#!@#
+
+
+// gl.uniform1i(uniformLocations.u_Tex,0)  // !@#!@#!@#
+
+// render
+// gl.viewport(0,0,width,height)  // !@#!@#!@#
+gl.bindTexture(gl.TEXTURE_2D,srcTex)//!@#!@#!@# 
+gl.drawArrays(gl.TRIANGLES,0,data.length/4)
+
+dstTex = new Uint8Array(size) 
+gl.readPixels(0,0,width,height,format,texType,dstTex)
+print('dstTex',dstTex)
+// !@#!@# what's the number of bytes in a float in glsl
